b4c73892404dea4bd4c3613ede8d0ac0ec18f1ed
Ashley Stanton-Nurse <git@analogrelay.net>
2023-12-20 08:40:20 -0800

    update secret reference

--- modules/k3s/_base.nix
+++ modules/k3s/_base.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  sops.secrets.k3s_token.sopsFile = ../secrets/secrets.yaml;
+  sops.secrets.k3s_token.sopsFile = ../../secrets/secrets.yaml;
 
   environment.systemPackages = [ pkgs.k3s ];
 
