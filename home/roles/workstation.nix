4941a14e1c01e337c4a9d34d422ee306ba8692a2
Ashley Stanton-Nurse <git@analogrelay.net>
2023-12-27 22:00:54 -0800

    move 1password config to workstation.darwin.nix

--- home/roles/workstation.nix
+++ home/roles/workstation.nix
@@ -11,9 +11,4 @@
   home.packages = with pkgs; [
     microsoft-edge-stable
   ];
-
-  # 1Password SSH agent
-  programs.ssh.matchBlocks."*".extraOptions = {
-    "IdentityAgent" = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
-  };
 }
