4bfd4268021753f3b0582944bba9a313f5668cce
Ashley Stanton-Nurse <git@analogrelay.net>
2024-01-17 10:20:57 -0800

    use port 22 for ssh on zach

--- machines/roles/workstation.wsl.nix
+++ machines/roles/workstation.wsl.nix
@@ -6,12 +6,12 @@
         listenAddresses = [ 
             {
                 addr = "0.0.0.0";
-                port = 2222;
+                port = 22;
             }
         ];
     };
 
     networking.firewall.allowedTCPPorts = [
-        2222 # sshd
+        22 # sshd
     ];
 }
