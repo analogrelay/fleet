1a1ddebc1bf118d02b6667d3323b805852b1e672
Ashley Stanton-Nurse <git@analogrelay.net>
2024-01-17 10:30:47 -0800

    taint 'zach' by default

--- machines/hosts/zach/default.nix
+++ machines/hosts/zach/default.nix
@@ -21,4 +21,9 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
+
+  services.k3s = {
+    # Mark this node so that it isn't scheduled on by default.
+    extraArgs = "--node-label transient=true:NoSchedule";
+  };
 }
