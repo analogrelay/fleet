3969800a68da224e959c458820ce2eeb7627eda2
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2024-06-17 13:15:36 -0700

    fix powershell ordering

--- machines/hosts/ashleyst-alpha/configure.ps1
+++ machines/hosts/ashleyst-alpha/configure.ps1
@@ -2,6 +2,6 @@ $RepoRoot = (Get-Item -Path $PSScriptRoot).Parent.Parent.Parent.FullName
 Import-Module "$RepoRoot\powershell\module\Fleet.psd1" -Scope Local
 
 # Set user-level environment variable to define key information about the machine.
-Set-MachineContext -Name "ashleyst-beta" -Role "workstation" -Realm "microsoft"
+Set-MachineContext -Name "ashleyst-alpha" -Role "workstation" -Realm "microsoft"
 
 Update-MachineConfiguration
