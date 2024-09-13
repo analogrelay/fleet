6a1630fb75ef0e4950f9cd23535e400e00071d59
Ashley Stanton-Nurse <github@analogrelay.net>
2024-09-13 16:35:49 +0000

    basic codespaces config
    Signed-off-by: Ashley Stanton-Nurse <ashleyst@microsoft.com>

--- home/profiles/shell.nix
+++ home/profiles/shell.nix
@@ -6,34 +6,13 @@
     dotDir = ".config/zsh";
     autosuggestion.enable = true;
     enableCompletion = true;
-
-    oh-my-zsh = {
-      enable = true;
-    };
-
-    initExtra = ''
-      # Mark all files with no extension in the 'functions' directory as autoloaded
-      FUNCS_TO_AUTOLOAD=("''${(@f)$(find "$HOME/.config/zsh/functions" \! -name "*.*")}")
-      for func in $FUNCS_TO_AUTOLOAD; do
-          autoload $func
-      done
-
-      # Load Git keys
-      if [ -f ~/.ssh/git_signing ]; then
-        ssh-add ~/.ssh/git_signing
-      fi
-    '';
-
-    shellAliases = {
-      ls = "eza";
-      cat = "bat";
-    };
-
-    profileExtra = ''
-    '';
   };
   home.file.".config/zsh/functions" = {
     source = ../../zsh/functions;
+    recursive = true;
+  };
+  home.file.".config/analogposh.omp.json" = {
+    source = ../analogposh.omp.json;
     recursive = true;
   };
 
@@ -43,8 +22,6 @@
 
   programs.oh-my-posh = {
     enable = true;
-    enableZshIntegration = true;
-    settings = builtins.fromJSON (builtins.unsafeDiscardStringContext (builtins.readFile ../analogposh.omp.json));
   };
 
   programs.starship = {
---
+++ zsh/.zshrc
@@ -0,0 +1,34 @@
+if [ ! -d /home/linuxbrew/.linuxbrew ]; then
+  echo "WARNING: Homebrew is not installed. Run 'script/bootstrap' in your dotfiles to install it."
+else
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+try_brew() {
+  if type -p brew > /dev/null; then
+    brew $@
+  fi
+}
+
+# Mark all files with no extension in the 'functions' directory as autoloaded
+FUNCS_TO_AUTOLOAD=("''${(@f)$(find "$HOME/.config/zsh/functions" \! -name "*.*")}")
+for func in $FUNCS_TO_AUTOLOAD; do
+    autoload $func
+done
+
+# Load Git keys
+if [ -f ~/.ssh/git_signing ]; then
+  ssh-add ~/.ssh/git_signing
+fi
+
+if type -p eza > /dev/null; then
+  alias ls="eza"
+fi
+
+if type -p bat > /dev/null; then
+  alias cat="bat"
+fi
+
+if type -p oh-my-posh > /dev/null; then
+  eval "$(oh-my-posh init zsh --config ~/.config/analogposh.omp.json)"
+fi
