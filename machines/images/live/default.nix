9db055eb216d500c9e53f6b2f6751b29b460ad20
Ashley Stanton-Nurse <git@analogrelay.net>
2025-03-10 11:42:32 -0700

    update to 6.13 kernel

--- machines/images/live/default.nix
+++ machines/images/live/default.nix
@@ -1,17 +1,32 @@
-{ modulesPath, ... }:
+{ modulesPath, pkgs, ... }:
 
 {
   imports = [
     (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
-
-    ../../platform.nix
-    ../../users/ashley.nix
   ];
-  networking.wireless.enable = false; # We use networkmanager in platform.nix
+
+  # Includes support for r8125 network driver
+  boot.kernelPackages = pkgs.linuxPackages_6_13;
+
+  networking.wireless.enable = false;
+  networking.networkmanager.enable = true;
+  systemd.services.NetworkManager-wait-online.enable = false;
   networking.hostName = "live";
-  home-manager.extraSpecialArgs = {
-    role = "image";
-    username = "ashley";
+
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
   };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    wget
+    curl
+    zsh
+    nix-index
+    tmux
+    nix
+    nixpkgs-fmt
+  ];
 }
 
