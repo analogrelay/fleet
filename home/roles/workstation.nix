04a3c76a191bab35340db6e240d26881cd9b46de
Ashley Stanton-Nurse <git@analogrelay.net>
2025-03-10 16:47:06 -0700

    fix git signing

--- home/roles/workstation.nix
+++ home/roles/workstation.nix
@@ -20,4 +20,7 @@
   programs.ssh.matchBlocks."*".extraOptions = {
     "IdentityAgent" = "~/.1password/agent.sock";
   };
+  programs.git.extraConfig = {
+    "gpg \"ssh\"".program = "op-ssh-sign";
+  };
 }
