4cc3e7341a70941b31476612357a29714340e5d3
Ashley Stanton-Nurse <git@analogrelay.net>
2025-03-12 09:52:19 -0700

    fix wsl git ssh command

--- home/roles/workstation.linux.nix
+++ home/roles/workstation.linux.nix
@@ -1,5 +1,10 @@
 { wsl, ... }:
 
 {
-  programs.git.extraConfig."gpg \"ssh\"".program = if wsl then "/mnt/c/Users/ashley/AppData/Local/1Password/app/8/op-ssh-sign-wsl" else "op-ssh-sign";
+  programs.git.extraConfig = if wsl then {
+    "gpg \"ssh\"".program = "/mnt/c/Users/ashley/AppData/Local/1Password/app/8/op-ssh-sign-wsl";
+    core.sshCommand = "ssh.exe";
+  } else {
+    "gpg \"ssh\"".program = "op-ssh-sign";
+  };
 }
