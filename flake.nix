b7be22496c2caac21305e5387401b192eed4776f
Ashley Stanton-Nurse <git@analogrelay.net>
2026-01-26 18:16:44 -0800

    temporary move nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "NixOS Configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    # Temporarily move to release-25.11 until the fix we need moves over to stable
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-analogrelay.url = "github:analogrelay/nixpkgs/analogrelay-main";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
@@ -29,24 +30,10 @@
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-unstable,
-      nixpkgs-analogrelay,
-      nixos-hardware,
-      flake-utils,
-      ssh-to-age,
-      nix-darwin,
-      sops-nix,
-      home-manager,
-      mac-app-util,
-      nix-vscode-extensions,
-      nixos-wsl,
-      vscode-server,
-      ...
-    }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixpkgs-analogrelay
+    , nixos-hardware, flake-utils, ssh-to-age, nix-darwin, sops-nix
+    , home-manager, mac-app-util, nix-vscode-extensions, nixos-wsl
+    , vscode-server, ... }@inputs:
     let
       inherit (self) outputs;
 
@@ -59,73 +46,52 @@
         sops-nix.nixosModules.sops
         home-manager.nixosModules.home-manager
         vscode-server.nixosModule
-        (
-          { config, pkgs, ... }:
-          {
-            services.vscode-server.enable = true;
-          }
-        )
+        ({ config, pkgs, ... }: { services.vscode-server.enable = true; })
       ];
       defaultDarwinModules = [ home-manager.darwinModules.home-manager ];
-      mkSpecialArgs =
-        system: platform:
+      mkSpecialArgs = system: platform:
         let
           pkgs-unstable = mkPkgsUnstable system;
           pkgs-analogrelay = mkPkgsAnalogrelay system;
-        in
-        {
-          inherit
-            inputs
-            outputs
-            platform
-            pkgs-unstable
-            pkgs-analogrelay
-            ;
-        };
-      mkPkgs =
-        system:
+        in { inherit inputs outputs platform pkgs-unstable pkgs-analogrelay; };
+      mkPkgs = system:
         import nixpkgs {
           inherit system overlays;
           config.allowUnfree = true;
         };
-      mkPkgsUnstable =
-        system:
+      mkPkgsUnstable = system:
         import nixpkgs-unstable {
           inherit system overlays;
           config.allowUnfree = true;
         };
-      mkPkgsAnalogrelay =
-        system:
+      mkPkgsAnalogrelay = system:
         import nixpkgs-analogrelay {
           inherit system overlays;
           config.allowUnfree = true;
         };
-      mkSystem =
-        system: extraModules:
+      mkSystem = system: extraModules:
         nixpkgs.lib.nixosSystem rec {
           inherit system;
           pkgs = mkPkgs system;
           modules = defaultModules ++ extraModules;
           specialArgs = mkSpecialArgs system "nixos";
         };
-      mkWslSystem =
-        system: extraModules:
+      mkWslSystem = system: extraModules:
         nixpkgs.lib.nixosSystem rec {
           inherit system;
           pkgs = mkPkgs system;
-          modules = defaultModules ++ [ nixos-wsl.nixosModules.wsl ] ++ extraModules;
+          modules = defaultModules ++ [ nixos-wsl.nixosModules.wsl ]
+            ++ extraModules;
           specialArgs = mkSpecialArgs system "wsl";
         };
-      mkDarwinSystem =
-        system: extraModules:
+      mkDarwinSystem = system: extraModules:
         nix-darwin.lib.darwinSystem rec {
           inherit system;
           pkgs = mkPkgs system;
           modules = defaultDarwinModules ++ extraModules;
           specialArgs = mkSpecialArgs system "darwin";
         };
-    in
-    rec {
+    in rec {
       lib = { inherit mkSystem; };
 
       nixosConfigurations = {
@@ -155,7 +121,8 @@
       };
       darwinConfigurations = {
         # MacBook Workstation
-        sephiroth = mkDarwinSystem "aarch64-darwin" [ ./machines/hosts/sephiroth ];
+        sephiroth =
+          mkDarwinSystem "aarch64-darwin" [ ./machines/hosts/sephiroth ];
       };
 
       homeConfigurations = {
@@ -186,35 +153,30 @@
       };
 
       overlays = import ./overlays { inherit inputs; };
-    }
-    // flake-utils.lib.eachDefaultSystem (
-      system:
+    } // flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
-      in
-      {
+      in {
         packages = import ./pkgs { pkgs = nixpkgs.legacyPackages.${system}; };
 
         formatter = nixpkgs.legacyPackages.${system}.alejandra;
 
         devShells.default = pkgs.mkShell {
-          packages =
-            [
-              ssh-to-age.packages.${system}.ssh-to-age
-              pkgs.zsh
-              pkgs.bashInteractive
-              pkgs.nix
-              pkgs.sops
-              pkgs.git
-              pkgs.jq
-              pkgs.nodejs
-              pkgs.python3
-              pkgs.yq
-            ]
-            ++ (pkgs.lib.lists.optional (pkgs.lib.strings.hasSuffix "-darwin" "${system}") [
-              nix-darwin.packages.${system}.darwin-rebuild
-            ]);
+          packages = [
+            ssh-to-age.packages.${system}.ssh-to-age
+            pkgs.zsh
+            pkgs.bashInteractive
+            pkgs.nix
+            pkgs.sops
+            pkgs.git
+            pkgs.jq
+            pkgs.nodejs
+            pkgs.python3
+            pkgs.yq
+          ] ++ (pkgs.lib.lists.optional
+            (pkgs.lib.strings.hasSuffix "-darwin" "${system}")
+            [ nix-darwin.packages.${system}.darwin-rebuild ]);
 
           shellHook = ''
             export FLEET_IN_SHELL=1
@@ -223,6 +185,5 @@
             pip install -r requirements.txt
           '';
         };
-      }
-    );
+      });
 }
