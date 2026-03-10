309877e1377955e8ee3a2b4411261e854df6171d
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2026-03-10 12:04:00 -0700

    feat: create config for ashleyst-omega

--- machines/hosts/ashleyst-omega/configure.ps1
+++ machines/hosts/ashleyst-omega/configure.ps1
@@ -2,6 +2,6 @@ $RepoRoot = (Get-Item -Path $PSScriptRoot).Parent.Parent.Parent.FullName
 Import-Module "$RepoRoot\powershell\module\Fleet.psd1" -Scope Local
 
 # Set user-level environment variable to define key information about the machine.
-Set-MachineContext -Name "cloud" -Role "workstation" -Realm "analoghome"
+Set-MachineContext -Name "ashleyst-omega" -Role "workstation" -Realm "microsoft"
 
 Update-MachineConfiguration
