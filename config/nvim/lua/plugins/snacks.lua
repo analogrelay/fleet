edd5bf8a8c6b68a6c6fc7a173f175cde578f8843
Ashley Stanton-Nurse <git@analogrelay.net>
2026-03-17 12:16:32 -0700

    feat(nvim): show hidden files in explorer by default

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -7,7 +7,7 @@ return {
 		opts = {
 			bigfile = { enabled = true },
 			dashboard = { enabled = true },
-			explorer = { enabled = true },
+			explorer = { enabled = true, hidden = true },
 			indent = { enabled = true },
 			input = { enabled = true },
 			notifier = {
