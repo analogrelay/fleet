7ddfafa00f6659344ea04e1c7116d69da9711d37
Ashley Stanton-Nurse <git@analogrelay.net>
2026-04-14 15:21:05 -0700

    feat: add jujutsu to default profile

--- home/default.nix
+++ home/default.nix
@@ -11,6 +11,7 @@ let
         ./profiles/agents.nix
         ./profiles/shell.nix
         ./profiles/git.nix
+        ./profiles/jujutsu.nix
         ./profiles/nvim.nix
         ./profiles/ssh.nix
         ./profiles/tmux.nix
